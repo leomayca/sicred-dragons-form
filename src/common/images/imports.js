e2a75f832fc7807b412d20a88d6e565457df6043
Leonardo Mayca <leomt8440@gmail.com>
2022-06-29 10:46:52 -0300

    create import images

---
+++ src/common/images/imports.js
@@ -0,0 +1,6 @@
+import redDragon from '../../assets/gif/red-dragon.gif'
+import fireRedDragon from '../../assets/gif/fire-red-dragon.gif'
+import backgroundWallpaper from '../../assets/jpg/background-wallpaper.jpg'
+import backgroundWallpaper2 from '../../assets/jpg/background-wallpaper2.jpg'
+
+export { redDragon, fireRedDragon, backgroundWallpaper, backgroundWallpaper2 }
