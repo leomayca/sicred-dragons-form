13dac15ba1e6a8898a00403962b7cf6fb9eec2c9
Leonardo Mayca <leomt8440@gmail.com>
2022-06-29 14:20:48 -0300

    create login page

--- src/pages/AuthPage/Login/Login.js
+++ src/pages/AuthPage/Login/Login.js
@@ -1,18 +1,76 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 
 import { AuthContext } from '../../../contexts/auth.context'
 
+import { Container, ImageContainer, FormContainer } from './styles'
+import { Input } from '../../../components/Input'
+import { Button } from '../../../components/Button'
+
 const Login = () => {
     const { handleLogin } = useContext(AuthContext)
+    const [login, setLogin] = useState({
+        user: '',
+        password: '',
+    })
+
+    const handleChange = (value, password) => {
+        if (password === true) {
+            setLogin({
+                ...login,
+                password: value,
+            })
+        } else {
+            setLogin({
+                ...login,
+                user: value,
+            })
+        }
+    }
 
     return (
         <React.Fragment>
-            <input type="text" />
-            <input type="password" />
-            <button type="button" onClick={handleLogin}>
-                Login
-            </button>
+            <Container>
+                <ImageContainer />
+                <FormContainer>
+                    <h1>Entrar no Sicred Dragons</h1>
+                    <Input
+                        placeholder="Usuário"
+                        handleChange={(e) => handleChange(e)}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                handleLogin(login)
+                            }
+                        }}
+                    />
+                    <Input
+                        password
+                        placeholder="Senha"
+                        handleChange={(e) => handleChange(e, true)}
+                        onKeyPress={(e) => {
+                            if (e.key === 'Enter') {
+                                handleLogin(login)
+                            }
+                        }}
+                    />
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'right',
+                            width: '100%',
+                            marginTop: '5px',
+                        }}
+                    >
+                        <Button
+                            style={{ width: '30%' }}
+                            onClick={() => handleLogin(login)}
+                        >
+                            Entrar
+                        </Button>
+                    </div>
+                </FormContainer>
+            </Container>
         </React.Fragment>
     )
 }
+
 export default Login
--- src/pages/AuthPage/Login/styles.js
+++ src/pages/AuthPage/Login/styles.js
@@ -1,1 +1,64 @@
 import styled from 'styled-components'
+
+import { backgroundDragon } from '../../../common/images'
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: 400px 1fr;
+    background-color: #121118;
+    width: 100vw;
+    height: 100vh;
+    grid-template-areas: 'form image';
+    overflow: hidden;
+
+    @media (max-width: 1024px) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
+export const ImageContainer = styled.div`
+    background-color: #000;
+    background-image: url(${backgroundDragon});
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    grid-area: image;
+    overflow: hidden;
+
+    @media (max-width: 1024px) {
+        display: flex;
+        height: 100%;
+    }
+`
+
+export const FormContainer = styled.div`
+    background-color: #111;
+    width: 100%;
+    grid-area: form;
+    overflow: hidden;
+    border-radius: 0 20px 20px 0;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 50px;
+
+    @media (max-width: 1024px) {
+        width: 370px;
+        height: 370px;
+        position: absolute;
+        border-radius: 20px;
+        background-color: rgba(17, 17, 17, 0.85);
+    }
+
+    h1 {
+        color: white;
+
+        @media (max-width: 1024px) {
+            font-size: 1.9em;
+        }
+    }
+`
